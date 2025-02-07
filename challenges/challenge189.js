e217c9d9f85e65493f5d8eeddaadaa9d70372d17
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2025-02-07 22:52:51 +0000

    feat[client]: added solution for challenge #189

---
+++ challenges/challenge189.js
@@ -0,0 +1,46 @@
+// 189. Rotate Array
+
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+// Example 1:
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+// Example 2:
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation:
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+
+var rotate = function (nums, k) {
+  while (k > 0) {
+    let last = nums.pop();
+    nums.unshift(last);
+    k--;
+  }
+  return nums;
+};
+
+var rotate1 = function (nums, k) {
+  const k = k % nums.length;
+
+  function helper(start, end) {
+    [nums[start], nums[end]] = [nums[end], nums[start]];
+
+    start++;
+    end--;
+  }
+
+  helper(0, nums.length - 1);
+  helper(0, k);
+  helper(k, nums.length - 1);
+
+  return nums;
+};
+
+// console.log(rotate1([1, 2, 3, 4, 5, 6, 7], 3));
